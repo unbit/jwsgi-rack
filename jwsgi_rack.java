39875c8c4d8c347ea17dcf301e599f86f6993e04
Roberta Giordano <roberta@20tab.com>
2013-06-02 08:45:27 +0200

    implemented full rack environ

--- jwsgi_rack.java
+++ jwsgi_rack.java
@@ -15,12 +15,91 @@ import org.jruby.runtime.CallBlock;
 import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.Block;
 import org.jruby.runtime.Arity;
+import org.jruby.anno.JRubyClass;
+import org.jruby.anno.JRubyMethod;
+import org.jruby.runtime.ObjectAllocator;
 
 	
 public class jwsgi_rack {
 
+	// the ruby instance
 	Ruby rb;
+	// the rack application
 	RubyObject app;
+	// the rack.input class
+	RubyClass rack_input;
+
+	@JRubyClass(name="Uwsgi_IO")
+	public class RackInput extends RubyObject {
+
+		public uwsgi.RequestBody input;
+
+		public RackInput(final Ruby ruby, RubyClass rubyClass) {
+			super(ruby, rubyClass);
+		}
+
+		@JRubyMethod(name="gets")
+		public IRubyObject gets(ThreadContext context) {
+			// unimplemented
+			return context.nil;
+		}
+
+		@JRubyMethod(name="each")
+		public IRubyObject each(ThreadContext context) {
+			// unimplemented
+			return context.nil;
+		}
+
+		@JRubyMethod(name="rewind")
+                public IRubyObject rewind(ThreadContext context) {
+			input.seek(0);
+			return context.nil;
+		}
+
+		@JRubyMethod(name="read",optional=2)
+		public IRubyObject read(ThreadContext context, IRubyObject [] args) {
+			long want = 0;
+			boolean return_nil = false;
+			// read all
+			if (args.length == 0) {
+				want = input.available();
+			}
+			else {
+				if (args[0] == context.getRuntime().getNil()) {
+					want = input.available();
+				}
+				else {
+					want = args[0].convertToInteger().getLongValue();
+					if (want == 0) {
+						want = input.available();
+					}
+					else {
+						return_nil = true;
+					}
+				}
+			}
+
+			System.out.println("need " + want);
+
+			byte[] b = new byte[(int)want];
+			int ret = input.read(b);
+			if (ret > 0) {
+				if (args.length > 1) {
+					System.out.println("size given");
+					RubyString buf = (RubyString) args[1];
+					buf.resize(ret);
+					buf.cat(b, 0, ret);
+					return buf;
+				}
+				System.out.println(RubyString.bytesToString(b, 0, ret));
+				return context.getRuntime().newString(RubyString.bytesToString(b, 0, ret));
+			}
+			if (return_nil) {
+				return context.nil;
+			}
+			return context.getRuntime().newString("");
+		}
+	}
 
 	static class HeadersBlock implements BlockCallback {
 		HashMap<String,Object> headers;
@@ -54,10 +133,10 @@ public class jwsgi_rack {
         }
 	
 	public jwsgi_rack() throws Exception {
+		// check for the virtual option
 		if (!uwsgi.opt.containsKey("jwsgi-rack")) {
 			throw new Exception("you have to specify the path of a rack application with the \"jwsgi-rack\" virtual option");	
 		}
-		System.out.println(uwsgi.opt.toString());
 		System.out.println("launching jruby...");
 		RubyInstanceConfig config = new RubyInstanceConfig();
 		rb = Ruby.newInstance();
@@ -65,6 +144,7 @@ public class jwsgi_rack {
 		config.getOutput().println(OutputStrings.getVersionString(config.getCompatVersion()));
 
 		rb.getLoadService().require("rubygems");
+		rb.getObject().callMethod("require", rb.newString("bundler/setup"));
 		rb.getObject().callMethod("require", rb.newString("rack"));
 
 		RubyModule rack_module = rb.getModule("Rack");
@@ -77,11 +157,22 @@ public class jwsgi_rack {
 		}
 		RubyArray rackup = (RubyArray) rack_builder.callMethod("parse_file", rb.newString((String)uwsgi.opt.get("jwsgi-rack")));
 		app = (RubyObject) rackup.get(0);
+
+		// we use Uwsgi_IO to be consistent with uWSGI rack plugin
+                rack_input = rb.defineClass("Uwsgi_IO", rb.getObject(), new ObjectAllocator() {
+                        public IRubyObject allocate(Ruby ruby, RubyClass klazz) {
+                                return new RackInput(ruby, klazz);
+                        }
+                });
+		// attach methods
+		rack_input.defineAnnotatedMethods(RackInput.class);
 	}
 
 	public Object[] application(HashMap env) throws Exception {
 
 		IRubyObject []no_args = { };
+
+		// fill the environ
 		RubyHash rb_env = new RubyHash(rb);
 		Iterator it = env.entrySet().iterator();
 		while(it.hasNext()) {
@@ -92,12 +183,12 @@ public class jwsgi_rack {
 				String str_key = (String) o_key;
 				String str_value = (String) o_value;
 				if (str_value.length() > 0 ||
-					str_key == "REQUEST_METHOD" ||
-					str_key == "SCRIPT_NAME" ||
-					str_key == "PATH_INFO" ||
-					str_key == "QUERY_STRING" ||
-					str_key == "SERVER_NAME" ||
-					str_key == "SERVER_PORT") {
+					str_key.equals("REQUEST_METHOD") ||
+					str_key.equals("SCRIPT_NAME") ||
+					str_key.equals("PATH_INFO") ||
+					str_key.equals("QUERY_STRING") ||
+					str_key.equals("SERVER_NAME") ||
+					str_key.equals("SERVER_PORT")) {
 					RubyString key = rb.newString( str_key );
 					RubyString value = rb.newString( str_value );
 					rb_env.put(key, value);
@@ -105,11 +196,32 @@ public class jwsgi_rack {
 			}
 		}
 
-		rb_env.put( rb.newString("rack.input"), rb.newString(""));
+		RackInput ri = (RackInput) rack_input.callMethod("new");
+		ri.input = (uwsgi.RequestBody) env.get("jwsgi.input");	
+		rb_env.put( rb.newString("rack.input"), ri);
+		rb_env.put( rb.newString("rack.errors"), rb.getGlobalVariables().get("$stderr"));
+		rb_env.put( rb.newString("rack.multithread"), rb.getTrue());
+		rb_env.put( rb.newString("rack.multiprocess"), rb.getTrue());
+		rb_env.put( rb.newString("rack.run_once"), rb.getFalse());
+		rb_env.put( rb.newString("rack.version"), rb.newArray(rb.newFixnum(1), rb.newFixnum(1)));
+		if (env.containsKey("UWSGI_SCHEME")) {
+			rb_env.put( rb.newString("rack.url_scheme"), rb_env.get(rb.newString("UWSGI_SCHEME")));
+		}
+		else if (env.containsKey("HTTPS")) {
+			rb_env.put( rb.newString("rack.url_scheme"), rb.newString("https"));
+		}
+		else {
+			rb_env.put( rb.newString("rack.url_scheme"), rb.newString("http"));
+		}
+		
 
+		// call the Rack function
 		RubyArray rb_response = (RubyArray) app.callMethod(rb.getCurrentContext(), "call", rb_env);
+
+		// get the status
 		long status = (Long) rb_response.get(0);
 
+		// get the headers
 		RubyObject rb_headers = (RubyObject) rb_response.get(1);
 		if (!rb_headers.respondsTo("each")) {
 			throw new Exception("Rack headers object must respond to \"each\"");
@@ -119,27 +231,30 @@ public class jwsgi_rack {
 		Block headers_iterator = CallBlock.newCallClosure(rb_headers, null, Arity.ONE_ARGUMENT, hb, rb.getCurrentContext());
 		rb_headers.callMethod(rb.getCurrentContext(), "each", no_args, headers_iterator);
 
+		// get the body
 		Object generic_body = null;
-
 		RubyObject body = (RubyObject) rb_response.get(2);
 		// use sendfile()
 		if (body.respondsTo("to_path")) {
 			RubyString rb_path = (RubyString) body.callMethod(rb.getCurrentContext(), "to_path");
 			generic_body = new File(rb_path.asJavaString());	
 		}
+		// normal string list
 		else if (body.respondsTo("each")) {
-		List<String> body_list = new ArrayList<String>();
-		BodyBlock bb = new BodyBlock(body_list);
-		Block body_iterator = CallBlock.newCallClosure(body, null, Arity.ONE_ARGUMENT, bb, rb.getCurrentContext());	
-		body.callMethod(rb.getCurrentContext(), "each", no_args, body_iterator);
-		generic_body = body_list;
-	}
+			List<String> body_list = new ArrayList<String>();
+			BodyBlock bb = new BodyBlock(body_list);
+			Block body_iterator = CallBlock.newCallClosure(body, null, Arity.ONE_ARGUMENT, bb, rb.getCurrentContext());	
+			body.callMethod(rb.getCurrentContext(), "each", no_args, body_iterator);
+			generic_body = body_list;
+		}
 
-	// call it always
-	if (body.respondsTo("to_close")) {
-	}
+		// call it always (if available)
+		if (body.respondsTo("close")) {
+			body.callMethod(rb.getCurrentContext(), "close");
+		}
 
-        Object[] response = { status, headers, generic_body };
-        return response;
-    }
+		// return the JWSGI response
+        	Object[] response = { status, headers, generic_body };
+        	return response;
+	}
 }
